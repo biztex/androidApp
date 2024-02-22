e17c56c056612677e742127038163750b9b472b6
biztex <luxuryman887@gmail.com>
2024-02-22 11:28:24 +0900

    added db initalizing function

--- app/src/main/java/com/biztex/manage/utils/DatabasesHelper.kt
+++ app/src/main/java/com/biztex/manage/utils/DatabasesHelper.kt
@@ -1,4 +1,16 @@
 package com.biztex.manage.utils
 
-class DatabasesHelper {
+import android.content.Context
+import androidx.room.Room
+import com.biztex.manage.db.AppDatabase
+
+class DatabasesHelper(context: Context) {
+    private val db: AppDatabase = Room.databaseBuilder(
+        context.applicationContext,
+        AppDatabase::class.java, "app_database"
+    ).fallbackToDestructiveMigration().build()
+
+    fun getDatabase(): AppDatabase{
+        return db
+    }
 }

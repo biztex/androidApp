27f5c793e1fb3aebf4fa08a2ec860c525dd72f84
biztex <luxuryman887@gmail.com>
2024-02-26 23:22:46 +0900

    added signup layout with firebase

---
+++ app/src/main/java/com/biztex/manage/ui/EmailPasswordAuthRegister.kt
@@ -0,0 +1,12 @@
+package com.biztex.manage.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.biztex.manage.R
+
+class EmailPasswordAuthRegister: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
